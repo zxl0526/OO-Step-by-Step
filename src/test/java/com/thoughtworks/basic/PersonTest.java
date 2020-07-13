b2b6aaaf9dd348d40560c9ed547487a94d159420
zxl0526 <851450219@qq.com>
2020-07-13 19:26:06 +0800

    can  introduce "My name is Tom. I am 21 years old."

---
+++ src/test/java/com/thoughtworks/basic/PersonTest.java
@@ -0,0 +1,20 @@
+package com.thoughtworks.basic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+public class PersonTest {
+    @Test
+    public void when_give_Name_IS_TOM_AGE_IS_21_should_introduce() {
+        //given
+        String name="Tom";
+        int age=21;
+        // when
+        String introduceResult=Person.introduce(name,age);
+        //then
+        assertEquals(introduceResult,"My name is Tom. I am 21 years old.");
+    }
+}
