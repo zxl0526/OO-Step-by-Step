47de13fdf6a50692cf7d97f50f418a00f7997cc1
zxl0526 <851450219@qq.com>
2020-07-14 10:33:44 +0800

    can  introduce "My name is Tom. I am 21 years old. I am a Student of Class 2."

--- src/main/java/com/thoughtworks/basic/Person.java
+++ src/main/java/com/thoughtworks/basic/Person.java
@@ -3,15 +3,60 @@ package com.thoughtworks.basic;
 /**
  * @author adminzxl
  */
-public class Person {
+public abstract class Person {
     private String name;
     private int age;
-    public  Person(String myName, int  myage) {
-        this.name = myName;
-        this.age = myage;
+    public int classNumber;
+
+    public String getName() {
+        return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public  Person(String myName, int myage) {
+        this.name = myName;
+        this.age = myage;
+
+       }
     public static String introduce(String name, int age) {
+        name="Tom";
+        age=21;
+        System.out.println("My name is "+name+",my age is "+age);
         return "My name is "+name+". I am " + age + " years old.";
     }
+
+    static class Student extends Person {
+        int classNumber;
+        Student(String name ,int age ,int classNumber){//子类构造方法
+            super(name,age);
+            this.classNumber = classNumber;
+        }
+
+        public static String  introduces(String name, int age, int classNumber){//显示班级
+
+            //System.out.println("My name is "+name+". I am " + age + " years old. I am a Student of Class" + classNumber);
+            return "My name is "+name+". I am " + age + " years old. I am a Student of Class " + classNumber;
+        }
+    }
+
+
 }
--- src/test/java/com/thoughtworks/basic/PersonTest.java
+++ src/test/java/com/thoughtworks/basic/PersonTest.java
@@ -7,21 +7,30 @@ import static org.junit.Assert.assertEquals;
 
 
 public class PersonTest {
-    private String name;
-    private int age;
-    public void Person(String myName, int  myage) {
-        this.name = myName;
-        this.age = myage;
-    }
 
     @Test
     public void when_give_Name_IS_TOM_AGE_IS_21_should_introduce() {
         //given
-        name="Tom";
-        age=21;
+        String name="Tom";
+        int age=21;
         // when
         String introduceResult=Person.introduce(name,age);
         //then
         assertEquals(introduceResult,"My name is Tom. I am 21 years old.");
     }
+    @Test
+    public void when_hava_Student_class_should_introduce() {
+        //given
+        String name="Tom";
+        int age=21;
+        int classNumber=2;
+        // when
+        String introduceResult=Person.Student.introduces(name,age,classNumber);
+        //then
+        assertEquals(introduceResult,"My name is Tom. I am 21 years old. I am a Student of Class 2");
+    }
+
+
+
+
 }
